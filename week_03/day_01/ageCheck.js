f586756b846f5d8b72cd7ca1d3e24d6e1d90000a
Harsha Khan <khan08raj@gmail.com>
2020-05-04 23:13:57 +0530

    final

---
+++ week_03/day_01/ageCheck.js
@@ -0,0 +1,20 @@
+function program(input) {
+    // input
+    if(input>18){
+        return("You can drive in India!")
+    }
+    else if(input<18){
+        return("You can't drive in India!")
+    }
+    else if(input === 18){
+        return("You are just old enough to drive in India!")
+    }
+    // console.log(input)
+    // write code here
+
+    
+}
+  
+
+
+module.exports = program
---
+++ week_03/day_01/countries.js
@@ -0,0 +1,57 @@
+function program(input) {
+   switch(input){
+       case "India":
+           output= "Asia";
+           break;
+        case "United States of America":
+            output="North America";
+            break;
+        case "China":
+            output="Asia";
+            break;
+        case "Nepal":
+            output="Asia";
+            break;
+        case "Australia":
+            output="Australia";
+            break;
+        case "France":
+            output="Europe";
+            break;
+        case "Germany":
+            output="Europe";
+            break;
+        case "South Africa":
+            output="Africa";
+            break;
+        case "Nigeria":
+            output="Africa";
+            break;
+        case "Mexico":
+            output="North America";
+            break;
+        case "Argentina":
+            output="South America";
+            break;
+        case "Brazil":
+            output="South America";
+            break;
+
+        case "Canada":
+            output = "North America";
+            break;
+        case "Singapore":
+            output= "Asia";
+            break;
+        case "Sweden":
+            output ="Europe";
+            break;
+
+
+
+   }
+    return (output)
+}
+
+
+module.exports = program
