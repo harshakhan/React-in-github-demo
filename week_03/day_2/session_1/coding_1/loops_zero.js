18e8eae9855809422907ae05f4bb564fd73efd19
Harsha Khan <khan08raj@gmail.com>
2020-05-06 22:00:46 +0530

    final

--- week_03/day_2/session_1/coding_1/loops_zero.js
+++ week_03/day_2/session_1/coding_1/loops_zero.js
@@ -1,7 +1,16 @@
 function program(input) {
     // input
+    var output=input
     // write code here
+    for(var i=input-1;i>=0;i--){
+        output += "\n"+i;
+        console.log(output)
+    }
+    if (input==0){
+        output='0'
+    }
     // return output
+    return(output)
 
 }
 
---
+++ week_03/day_2/session_1/coding_1/test.js
@@ -0,0 +1,11 @@
+function program(input) {
+    var output='0'
+    // console.log(output)
+    for(var i=1;i<=input;i--){
+        output+="\n"+i;
+        console.log(output)
+    }
+    return(output)
+
+}
+input=10
