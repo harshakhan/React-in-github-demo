958b2deff5e34baeb033ac5260222d6866a78377
Harsha Khan <khan08raj@gmail.com>
2020-05-08 20:44:05 +0530

    final

--- week_03/day_2/session_2/coding_2/join_array.js
+++ week_03/day_2/session_2/coding_2/join_array.js
@@ -1,9 +1,10 @@
 function program(input,char) {
     // input, char
     // console.log(input)
-    output =input.split("")
+    output =input.split(" ")
     // console.log(output)
     // write code here
+    
     console.log(output.join(char));
     
     
--- week_03/day_2/session_2/coding_2/reverse_string.js
+++ week_03/day_2/session_2/coding_2/reverse_string.js
@@ -1,6 +1,16 @@
 function program(input) {
     // input
     // write code here
+    var arr=input.split('')
+    var temp
+    // console.log(arr)
+    for(var i=0;i<arr.length/2;i++){
+        temp=arr[arr.length-1-i]
+        arr[arr.length-1-i]=arr[i]
+        arr[i]=temp
+    }
+    return arr.join('')
+
 
 }
 
