b06ba8ffe3c7b5799c110bcba69105cd0c5869e3
Harsha Khan <khan08raj@gmail.com>
2020-05-10 01:29:04 +0530

    final

--- week_03/day_2/session_2/coding_2/average_divisible.js
+++ week_03/day_2/session_2/coding_2/average_divisible.js
@@ -1,8 +1,26 @@
 function program(input,n) {
     // input
     // write code here
+    input=Number(input)
+    n=Number(n)
+    var sumNum=0,avgNum=0,countNum=0;
+
+    
+    for(var i=1;i<=input;i++)
+    {
+        if(i%n==0){
+            sumNum+=i
+            countNum+=1
+           
+        }
+        
+    }
+    avgNum=sumNum/countNum;
+    return avgNum
     
 }
+    
+
 
 
 module.exports = program
--- week_03/day_2/session_2/coding_2/count_consonants.js
+++ week_03/day_2/session_2/coding_2/count_consonants.js
@@ -1,6 +1,19 @@
 function program(input) {
     // input
-    // write code here
+    input=input.toLowerCase()
+   
+    // console.log(output)
+    
+    
+   var consants=0
+   for(var i=0;i<input.length;i++){
+
+
+       if(input[i] !== "a" || input[i] !== "e" || input[i] !== "i" || input[i] !== "o" || input[i] !== "u" || input[i] !== " "){
+           consants++
+       }
+   }
+   return consants  
 
 }
 
