8c581084615a122c2c45168fece9e5e9d8a48354
Harsha Khan <khan08raj@gmail.com>
2020-05-09 21:37:44 +0530

    final

--- week_03/day_2/session_2/coding_2/count_vowels.js
+++ week_03/day_2/session_2/coding_2/count_vowels.js
@@ -1,6 +1,15 @@
 function program(input) {
     // input
     // write code here
+    input=input.split('')
+    var vowl_list="aeiouAEIOU"
+    var count=0
+    for (var i=0;i<input.length;i++){
+        if(vowl_list.indexOf(input[i])!==-1){
+            count+=1
+        }
+    }
+    return count
 
 }
 
