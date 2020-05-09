4c4a02ed2e435fb4d0e653a79ff27569192e574b
Harsha Khan <khan08raj@gmail.com>
2020-05-10 01:50:25 +0530

    final

--- week_03/day_2/session_2/coding_2/count_consonants.js
+++ week_03/day_2/session_2/coding_2/count_consonants.js
@@ -5,15 +5,19 @@ function program(input) {
     // console.log(output)
     
     
-   var consants=0
+   var count=0
    for(var i=0;i<input.length;i++){
 
 
-       if(input[i] !== "a" || input[i] !== "e" || input[i] !== "i" || input[i] !== "o" || input[i] !== "u" || input[i] !== " "){
-           consants++
+       if(input[i] == "a" || input[i] == "e" || input[i] == "i" || input[i] == "o" || input[i] == "u" || input[i] == 1 || input[i] == 2 || input[i] == 3 || input[i] == 4 || 
+          input[i] == 5 || input[i] == 6 || input[i] == 7 || input[i] == 8 || input[i] == 9){
+           count+=0
+       }
+       else{
+           count+=1
        }
    }
-   return consants  
+   return count 
 
 }
 
