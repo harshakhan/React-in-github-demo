53e4c8f51af0436fdb9b56e61013cd9650346f29
Harsha Khan <khan08raj@gmail.com>
2020-05-10 02:10:36 +0530

    final

--- week_03/day_2/session_2/coding_2/average_divisible_while.js
+++ week_03/day_2/session_2/coding_2/average_divisible_while.js
@@ -1,6 +1,25 @@
 function program(input,n) {
     // input
     // write code here
+    input=Number(input)
+    n=Number(n)
+    var sumNum=0,avgNum=0,countNum=0;
+    let i=1;
+
+    
+    while(i<=input)
+    {
+        if(i%n==0){
+            sumNum+=i
+            countNum+=1
+           
+        }
+        i++;
+        
+    }
+    avgNum=sumNum/countNum;
+    return avgNum
+    
 
 }
 
