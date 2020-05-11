4ca07fdd2206e4731080ec70fffe3b6d8ffd6593
Harsha Khan <khan08raj@gmail.com>
2020-05-12 02:24:08 +0530

    final

--- week_03/day_6/assignments/subset.js
+++ week_03/day_6/assignments/subset.js
@@ -2,6 +2,23 @@ function program(input) {
     // input
     // write code here
     //  return output
+    input=input.split('\n')
+    num1=input[0].split(" ")
+    num2=input[1].split(" ")
+    var count =0;
+    for(var i=0;i<num1.length;i++){
+        for(var j=0;j<num2.length;j++){
+            if(Number(num1[i])===Number(num2[j])){
+                count +=1
+            }
+        }
+    }
+    if(count>=num1.length){
+        return "YES"
+    }
+    else{
+        return "NO"
+    }
 }
 
 
