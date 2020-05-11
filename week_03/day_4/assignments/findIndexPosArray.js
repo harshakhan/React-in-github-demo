c579f4792709a0f5f4a637695273997d8c6dc9e6
Harsha Khan <khan08raj@gmail.com>
2020-05-11 08:01:27 +0530

    final

--- week_03/day_4/assignments/findIndexPosArray.js
+++ week_03/day_4/assignments/findIndexPosArray.js
@@ -2,6 +2,16 @@ function program(input) {
     // input
     // write code here
     //  return output
+    input=input.split('\n')
+    var num=Number(input[1])
+    array=input[0].split(' ')
+    for (var i=0;i<array.length;i++){
+        if(Number(array[i])== num){
+            return(i)
+        }
+       
+    }
+    return -1
 }
 
 
--- week_03/day_4/assignments/removeDuplicatesArrays.js
+++ week_03/day_4/assignments/removeDuplicatesArrays.js
@@ -1,21 +1,7 @@
 function program(input) {
     // input
     // write code here
-    //  return output
-    input=input.split('\n')
-    var target=Number(input[1])
-    input=input[0].split('')
-    var count=0
-    for(var i=0;i<input.length;i++){
-        if(Number(Number(input[i])==target)){
-            count+=1
-        }
-        console.log(count)
-    }
-    // if(count ===1){
-    //     return(count=-1)
-    // }
-    // return(count)
+   
 }
 
 
