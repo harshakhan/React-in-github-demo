05428583b74e59fa51c8ed783e2bf4fb6aaee87d
Harsha Khan <khan08raj@gmail.com>
2020-05-12 02:50:29 +0530

    final

--- week_03/day_4/assignments/removeDuplicatesArraysObjects.js
+++ week_03/day_4/assignments/removeDuplicatesArraysObjects.js
@@ -2,6 +2,14 @@ function program(input) {
     // input
     // write code here
     //  return output
+    input= input.split(' ').map(Number);
+    var  obj={}
+    for(var i=0;i<input.length;i++){
+        var elem =input[i]
+        obj[elem]=elem
+    }
+    console.log(obj)
+    return Object.values(obj).join(' ')
 }
 
 
