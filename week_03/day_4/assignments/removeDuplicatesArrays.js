9c179598ed7aaff6954f64d399ede826be4e1155
Harsha Khan <khan08raj@gmail.com>
2020-05-11 08:24:55 +0530

    final

--- week_03/day_4/assignments/removeDuplicatesArrays.js
+++ week_03/day_4/assignments/removeDuplicatesArrays.js
@@ -1,6 +1,31 @@
+function isDuplicate(arr,target){
+    var flag=false;
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]===target){
+            flag= true;
+            return flag
+        }
+    }
+    return flag
+}
+
+
+
+
+
 function program(input) {
     // input
+    
     // write code here
+    input=input.split(' ')
+    var out=[]
+    for(var i=0;i<input.length;i++){
+        if(!isDuplicate(out,input[i])){
+            out.push(input[i])
+        }
+    }
+    console.log(out.join(' '))
+    return out.join(' ')
    
 }
 
