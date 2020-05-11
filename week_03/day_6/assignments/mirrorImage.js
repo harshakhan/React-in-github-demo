cd730c79c3f6282c562b01cc27ff5dcc09eaba58
Harsha Khan <khan08raj@gmail.com>
2020-05-12 01:50:39 +0530

    finish

--- week_03/day_6/assignments/mirrorImage.js
+++ week_03/day_6/assignments/mirrorImage.js
@@ -2,6 +2,28 @@ function program(input) {
     // input
     // write code here
     //return output
+    var out=""
+    for(var i=0;i<input.length;i++){
+        out += mirror2(input[i])
+    }
+    return(out)
+}
+function mirror2(char){
+    var lower=('abcdefghijklmnopqrstuvwxyz')
+    var upper=('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    revUpper=('ZYXWVUTSRQPONMLKJIHGFEDCBA')
+    revLower=('zyxwvutsrqponmlkjihgfedcba')
+    for(var i=0; i<lower.length;i++){
+        if(lower[i]===char){
+            return(revLower[i])
+
+
+        }
+        else if(upper[i]===char){
+            return(revUpper[i])
+        }
+    }
+    return(char)
 }
 
 
