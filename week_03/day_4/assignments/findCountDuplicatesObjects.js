14d9a98471278dd968e1a35d15a79934153c26e5
Harsha Khan <khan08raj@gmail.com>
2020-05-15 01:18:27 +0530

    final

--- week_03/day_4/assignments/findCountDuplicatesObjects.js
+++ week_03/day_4/assignments/findCountDuplicatesObjects.js
@@ -2,6 +2,29 @@ function program(input) {
     // input
     // write code here
     //  return output
+    input=input.split('\n')
+    var target = Number(input[1])
+    input = input[0].split(' ')
+    var count =0
+    var obj ={}
+    for(var i =0; i<input.length; i++){
+        var elem = input[i]
+        if(!(obj[elem])){
+            obj[elem]=1
+        }
+        else if (obj[elem]){
+            obj[elem]+= 1
+
+        }
+        
+        
+    }
+    if(obj[target]){
+        return (obj[target])
+    }
+    else{
+        return -1
+    }
 }
 
 
--- week_03/day_4/assignments/findIndexPosObjects.js
+++ week_03/day_4/assignments/findIndexPosObjects.js
@@ -1,7 +1,29 @@
 function program(input) {
     // input
     // write code here
-    //  return output
+    //  return outputi
+
+    input=input.split('\n')
+    var target =Number(input[1])
+    input = input[0].split(' ').map(Number)
+
+    var obj ={}
+    for(var i=0; i<input.length; i++){
+        var elem = input[i]
+        if(obj[elem]){
+            obj[elem].push(i)
+        }
+        else{
+            obj[elem]=[i]
+        }
+        
+    }
+    if(obj[target]){
+        return obj [target]
+    }
+    else{
+        return -1
+    }
 }
 
 
