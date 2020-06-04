e66a99799b546309be4e4b5e731d8086a80a6ecc
Harsha Khan <khan08raj@gmail.com>
2020-06-05 02:21:56 +0530

    final

---
+++ week_07/day_4/assignments/higherorder.js
@@ -0,0 +1,94 @@
+// 1. Given an array of numbers print the cube of those numbers (map)
+// 2. Given an array of numbers print the numbers divisible by 3 (filter)
+// 3. Given an array of numbers print the product of all numbers (reduce)
+// 4. Given an array of strings print the length of each string (map)
+// 5. Given an array of strings print the strings whose first or last letter is `a` (filter)
+// 6. Given an array of strings print the snake case of the strings (reduce) [Assume Small case strings]
+// 7. Given an array of numbers find the sum of cubes if the number is divisible by 3
+// 8. Given an array of strings print the sum of lengths if the characters in the string are odd
+
+
+// var nums=[1,2,3,4,5,6,7,8,9,10]
+
+// var nums_cube =nums.map(function(elements){
+//     return elements*elements*elements
+// })
+
+// console.log(nums_cube)
+
+//-------------------------------------------------
+// var nums=[1,2,3,4,5,6,7,8,9,10]
+// var is_divisible=nums.filter(function(elements){
+//     return elements %3 ==0
+// })
+// console.log(is_divisible)
+
+//------------------------------------------------------------------------------
+
+// var nums=[1,2,3,4,5,6,7,8,9,10]
+// var product = nums.reduce(function(prod,elements){
+//     return prod * elements
+
+// })
+// console.log(product)
+
+//------------------------------------------------------------------------------
+
+// var str=["harsha", "aditi", "tanushre","Ankur","Ankita","Lisa","Sahini"]
+
+// var length=str.map(function(elements){
+//     return elements.length
+// })
+// console.log(length)
+
+
+
+//----------------------------------------------------------------------
+//  var str=["harsha", "aditi", "tanushre","Aditi","Ankita",'Anupam','Diya','Sayak']
+
+// var a_letter = str.filter(function(elements){
+//     return (elements[0]=="A" || elements[elements.length-1]=="a")
+// })
+// console.log(a_letter)
+//-------------------------------------------------------
+
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+
+// var find_nums=arr.filter(function(elements){
+//     return elements%3==0
+// })
+
+
+
+// var find_cubes=find_nums.map(function(elements){
+//     return elements*elements*elements
+// })
+// console.log(find_cubes)
+
+//---------------------------------------------------------
+
+var str= ['ankur','rites','nurpul','ritesh','yogesh','albert','avishek']
+
+var arr=[]
+for(var i=0;i<str.length;i++){
+    if(i%2==0){
+        var ans= (str[i])
+        arr.push(ans)
+    }
+}
+console.log(arr)
+
+var length =arr.map(function(elements){
+    return elements.length
+})
+console.log(length)
+
+var sum_total =length.reduce(function(sum,elements){
+    return sum+elements
+})
+console.log(sum_total)
+
+//------------------------------------------
+
+
+
