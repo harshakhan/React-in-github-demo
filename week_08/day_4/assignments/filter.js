5ff31cee16b0b8c563fe473a454ad29709a2ce4e
Harsha Khan <khan08raj@gmail.com>
2020-06-12 02:41:06 +0530

    final

---
+++ week_08/day_4/assignments/filter.js
@@ -0,0 +1,93 @@
+var arr =[] 
+
+window.onload = function(){
+    var form = document.querySelector('form');
+    form.addEventListener('submit',handleSubmit)
+
+
+    var filter = document.querySelector('#filter')
+    filter.addEventListener('change',handleFilter)
+}
+
+
+function handleSubmit(){
+    event.preventDefault();
+    var elems= event.target.querySelectorAll(".input");
+    var data = getFormData(elems)
+
+    arr.push(data) // adding the data to global array
+    renderDOM(arr)
+}
+
+function getFormData(elems){
+    var formData ={}
+    var len = elems.length
+    for(var i=0;i<len;i++){
+        var key = elems[i].name
+        formData[key] = elems[i].value
+    }
+    return formData
+//    console.log(formData)
+
+
+}
+
+function renderDOM(arr){
+
+    var div = document.querySelector("#res")
+    div.innerHTML=""
+
+    var cont = document.createElement('div')
+
+    var len = arr.length
+    for(var i =0;i<len;i++){
+        var row = createRow(arr[i]) //invoking another fun to create
+        cont.append(row)  //  appending the row into cont
+    }
+    div.append(cont)  //appending the cont into div.
+}
+
+function createRow(data){
+    var name = data.name
+    var salary = data.salary
+    var dept = data.department
+    var loc = data.location
+
+
+
+    var div = document.createElement('div')
+    div.setAttribute('class','row-item')
+
+
+
+    var nameElem = document.createElement('p')
+    nameElem.textContent= name
+
+    var salaryElem = document.createElement('p')
+    salaryElem.textContent= salary
+
+    var deptElem = document.createElement('p')
+    deptElem.textContent= dept
+
+
+    var locElem = document.createElement('p')
+    locElem.textContent= loc
+
+
+    div. append(nameElem,salaryElem,deptElem,locElem)
+
+    return div
+
+
+}
+
+
+function handleFilter(){
+    var department = event.target.value
+
+    console.log(department)
+    var newArr = arr.filter(function(item){
+        return item.department === department
+    })
+    renderDOM(newArr)
+}
