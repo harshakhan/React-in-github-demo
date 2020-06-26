2bab6338f7d07c63c0ef5531ac28536e3a2d4001
Harsha Khan <khan08raj@gmail.com>
2020-06-26 23:44:39 +0530

    api

--- week_10/day_2/session_1/practice1_api.js
+++ week_10/day_2/session_1/practice1_api.js
@@ -1,92 +1,5 @@
-window.onload = function(){
-    getUserlist()
-
-}
-
-
-function getUserlist(){
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET','https://reqres.in/api/users?page=2')
-
-    xhr.send()
-    xhr.onload = function(){
-        // console.log(this.response,typeof this.response)
-        var data = JSON.parse(this.response)     // interacting with sersver by replacing the XML with JSON (its converting its into objects)
-        
-        handleResponse(data)
-
-    }
-}
-
-
-function handleResponse(data){    // varible name is there and we are passing data.data
-    renderDOM(data.data)
-
-}
-
-
-function renderDOM(arr){
-    document.body.innerHTML =""
-    var div = document.createElement('div');
-    var ul = document.createElement('ul');
-
-    for(var i=0;i<arr.length;i++){
-        var li = document.createElement('li')
-        li.textContent = "email: " +arr[i].email + ",Name" + arr[i].first_name;
-        ul.append(li)
-    }
-    div.append(ul)
-    document.body.append(div)
-
-
-
-}
-
-//----------------------------------------------------------------------------------------------
-
-
 // window.onload = function(){
-//     var getData = document.getElementById('get')
-//     getData.addEventListener('click',getUserlist)
-
-//     var form = document.querySelector('form')
-//     form.addEventListener("submit",loginUser)
-
-
-  
-    
-
-// }
-
-
-// function loginUser(){
-//     event.preventDefault()
-
-//     var elems = event.target.querySelectorAll('input')
-    
-//     var username = elems[0].value                     // we are just hardcoding the value
-//     var password = elems[1].value
-
-
-//     var payload = {
-//         username: username,
-//         password:password
-//     }
-
-//     console.log(username,password)   // it will print the output to console
-
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('POST','https://reqres.in/api/login')
-
-//     xhr.onload = function(){
-//         console.log(this.response)
-//     }
-
-//     xhr.setRequestHeader('Content-Type','application/json;charset=utf-8')  // declaring what type of XMLHttp is
-
-//     xhr.send(JSON.stringify(payload))    //we just cant push the ocject data directly into payload so we need to do stringfy( we will get error before so to remove error we need to specify what kind of data it is)
-
+//     getUserlist()
 
 // }
 
@@ -100,10 +13,8 @@ function renderDOM(arr){
 //     xhr.onload = function(){
 //         // console.log(this.response,typeof this.response)
 //         var response = JSON.parse(this.response)     // interacting with sersver by replacing the XML with JSON (its converting its into objects)
-        
-//         // console.log(data.data)
+//         console.log(response)   // it will directly print the data inside the data.
 //         handleResponse(response)
-
 
 //     }
 // }
@@ -115,10 +26,8 @@ function renderDOM(arr){
 // }
 
 
-// function renderDOM(arr){
-   
-//     var res = document.getElementById('res')
-//     res.innerHTML=""
+// function renderDOM(arr){   //arr is here is a key
+//     document.body.innerHTML =""
 //     var div = document.createElement('div');
 //     var ul = document.createElement('ul');
 
@@ -128,11 +37,102 @@ function renderDOM(arr){
 //         ul.append(li)
 //     }
 //     div.append(ul)
-//     res.append(div)
+//     document.body.append(div)
 
 
 
 // }
+
+//----------------------------------------------------------------------------------------------
+
+
+window.onload = function(){
+    var getData = document.getElementById('get')
+    getData.addEventListener('click',getUserlist)
+
+    var form = document.querySelector('form')
+    form.addEventListener("submit",loginUser)
+
+
+  
+    
+
+}
+
+
+function loginUser(){
+    event.preventDefault()
+
+    var elems = event.target.querySelectorAll('input')
+    
+    var username = elems[0].value                     // we are just hardcoding the value
+    var password = elems[1].value
+
+
+    var payload = {
+        username: username,
+        password:password
+    }
+
+    console.log(username,password)   // it will print the output to console
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST','https://reqres.in/api/login')
+
+    xhr.onload = function(){
+        console.log(this.response)
+    }
+
+    xhr.setRequestHeader('Content-Type','application/json;charset=utf-8')  // declaring what type of XMLHttp is
+
+    xhr.send(JSON.stringify(payload))    //we just cant push the ocject data directly into payload so we need to do stringfy( we will get error before so to remove error we need to specify what kind of data it is)
+
+
+}
+
+
+function getUserlist(){
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET','https://reqres.in/api/users?page=2')
+
+    xhr.send()
+    xhr.onload = function(){
+        // console.log(this.response,typeof this.response)
+        var response = JSON.parse(this.response)     // interacting with sersver by replacing the XML with JSON (its converting its into objects)
+        
+        // console.log(data.data)
+        handleResponse(response)
+
+
+    }
+}
+
+
+function handleResponse(data){    // varible name is there and we are passing data.data
+    renderDOM(data.data)
+
+}
+
+
+function renderDOM(arr){
+   
+    var res = document.getElementById('res')
+    res.innerHTML=""
+    var div = document.createElement('div');
+    var ul = document.createElement('ul');
+
+    for(var i=0;i<arr.length;i++){
+        var li = document.createElement('li')
+        li.textContent = "email: " +arr[i].email + ",Name" + arr[i].first_name;
+        ul.append(li)
+    }
+    div.append(ul)
+    res.append(div)
+
+
+
+}
 
 
 // // we will get username and password after completion of the code and with token code all will be shown in the console
@@ -292,9 +292,13 @@ function renderDOM(arr){
 //             handleSuccessLogin(JSON.parse(this.response))
 //         }
 //         else{
-//             alert('error')
+           
 //             handleError(JSON.parse(this.response))
 //         }
+//     }
+
+//     xhr.onerror = function(){
+//         alert('oneerror')
 //     }
 
 //     xhr.setRequestHeader('Content-Type','application/json;charset=utf-8')  // declaring what type of XMLHttp is
@@ -325,7 +329,7 @@ function renderDOM(arr){
 // function getUserlist(){
 //     var xhr = new XMLHttpRequest();
 
-//     xhr.open('GET','https://reqres.in/api/users?page=2')
+//     xhr.open('GET','https://reqres.in/api/users?page=1')
 
 //     xhr.send()
 //     xhr.onload = function(){
---
+++ week_10/day_4/assignments/api.js
@@ -0,0 +1,50 @@
+window.onload = function(){
+    var form  = document.querySelector("form")
+    form.addEventListener('submit',handleForm)
+}
+
+
+function handleForm(){
+    event.preventDefault()
+
+    var input = document.getElementById('os').value
+    // console.log(input)
+
+    var xhr = new XMLHttpRequest()
+
+    xhr.open('GET','http://localhost:8080/codenames/'+ input)
+    xhr.send()
+
+    xhr.onload = function(){
+        var data = JSON.parse(this.response)
+
+        // console.log(data)
+        handleResponseRequest(data)
+    }
+}
+
+
+function handleResponseRequest(data){
+    console.log(data.codenames)
+
+    renderDOM(data.codenames)
+
+    
+}
+
+function renderDOM(arr){
+    var res = document.getElementById('res')
+    res.innerHTML=""
+
+    var div = document.createElement('div')
+
+    var ul = document.createElement('ul')
+    for(var i =0;i<arr.length;i++){
+        var li= document.createElement('li')
+        li.textContent="version:" + arr[i].version + "name:"+ arr[i].name
+        ul.append(li)
+    }
+    div.append(ul)
+    res.append(div)
+}
+
