e5c4471d3868bfd938b84648a2ed70afd72df755
Harsha Khan <khan08raj@gmail.com>
2020-07-24 02:49:11 +0530

    countries

--- week_13/day_6/assignments/countries.js
+++ week_13/day_6/assignments/countries.js
@@ -1,15 +1,50 @@
-const India
-const United States of America
-const China
-const Nepal
-const Australia
-const France
-const Germany
-const South Africa
-const Nigeria
-const Mexico
+const INDIA='India'
+const USA= 'United States of America'
+const CHINA='China'
+const NEPAL='Nepal'
+const AUSTRALIA ='Australia'
+const FRANCE ='France'
+const GERMANY='Germany'
+const SOUTH_AFRICA='South Africa'
+const NIGERIA ='Nigeria'
+const MEXICO='Mexico'
 const ARGENTIA='Argentina'
 const BRAZIL='Brazil'
 const CANADA= 'Canada'
 const SINGAPORE ="Singpore"
 const SWEDEN ="Europe"
+
+switch(BRAZIL){
+    case 'India':console.log('Asia')
+    break
+    case 'United States of America':console.log('North America')
+    break
+    case 'China':console.log('Asia')
+    break
+    case 'Nepal':console.log('Asia')
+    break
+    case 'Australia':console.log('Australia')
+    break
+    case 'France':console.log('Europe')
+    break
+    case 'Germany':console.log('Europe')
+    break
+    case 'South Africa':console.log('Africa')
+    break
+    case 'Nigeria':console.log('Africa')
+    break
+    case 'Mexico':console.log('Nort America')
+    break
+    case 'Argentinaa':console.log('South America')
+    break
+    case 'Brazil':console.log('South America')
+    break
+    case 'Canada':console.log('South America')
+    break
+    case 'Singpore':console.log('North America')
+    break
+    case 'Europe':console.log('Asia')
+    break
+    default : console.log('Europe')
+
+}
