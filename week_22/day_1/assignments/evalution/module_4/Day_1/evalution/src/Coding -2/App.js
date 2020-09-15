55d835ca60cf957991cf93379111d7bc64f559e1
harshakhan <harshakhan08@gmail.com>
2020-09-15 15:16:49 +0530

    evalution

--- week_22/day_1/assignments/evalution/module_4/Day_1/evalution/src/Coding -2/App.js
+++ week_22/day_1/assignments/evalution/module_4/Day_1/evalution/src/Coding -2/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import FormWrapper from "./Component/FormWrapper";
 
-import './App.css';
 
 
 function App() {
--- week_22/day_1/assignments/evalution/module_4/Day_1/evalution/src/Coding -2/Component/TableItem.jsx
+++ week_22/day_1/assignments/evalution/module_4/Day_1/evalution/src/Coding -2/Component/TableItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styles from "./form.module.css";
 class TableItem extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +11,11 @@ class TableItem extends React.Component {
     };
   }
 
+  // toggleEdit = () => {
+  //   this.setState({
+  //     isEdit: !this.state.isEdit
+  //   });
+  // };
 
   handleEdit = (e) => {
     let { name, value: val, type, checked } = e.target;
@@ -36,12 +41,43 @@ class TableItem extends React.Component {
       location,
       salary,
       remote
-     
+      // handleDelete
     } = this.props;
+
+    // const { isEdit, value } = this.state;
+
+    // if (isEdit) {
+    //   return (
+    //     <>
+    //       <div style={{ display: "flex", flex: "1" }}>
+    //         <input
+    //           onChange={this.handleEdit}
+    //           style={{ flex: "1" }}
+    //           name="name"
+    //         ></input>
+    //         <input
+    //           onChange={this.handleEdit}
+    //           style={{ flex: "1" }}
+    //           name="date"
+    //         ></input>
+    //         <input onChange={this.handleEdit} style={{ flex: "1" }}></input>
+    //         <div style={{ flex: "1" }}>{location}</div>
+    //         <div style={{ flex: "1" }}>{salary}</div>
+    //         <div style={{ flex: "1" }}>{remote ? "Yes" : "No"}</div>
+    //         <button onClick={this.handleUpdate} style={{ flex: "1" }}>
+    //           Update
+    //         </button>
+    //         <button onClick={this.toggleEdit} style={{ flex: "1" }}>
+    //           Cancel
+    //         </button>
+    //       </div>
+    //     </>
+    //   );
+    // }
 
     return (
       <>
-        <div style={{ border: "1px solid  black", margin: "20px" ,padding:"10px"}}>
+        <div style={{ border: "1px solid  black", margin: "20px" }}>
           <div style={{ display: "flex", flex: "1" }}>
             <div style={{ flex: "1" }}>
               <p>Title</p>
@@ -65,9 +101,14 @@ class TableItem extends React.Component {
             </div>
             <div style={{ flex: "1" }}>
               <p>Remote</p>
-              {remote ? "Yes" : "No"}
+              <div className ={remote ? styles.green : styles.red}></div>
             </div>
-            
+            {/* <button onClick={handleDelete} style={{ flex: "1" }}>
+            DELETE
+          </button> */}
+            {/* <button onClick={() => this.toggleEdit()} style={{ flex: "1" }}>
+            EDIT
+          </button> */}
           </div>
         </div>
       </>
