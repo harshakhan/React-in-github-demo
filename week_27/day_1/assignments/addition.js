3a7b7f6f86ef2599b5301cbe173f9d09d37707a6
harshakhan <harshakhan08@gmail.com>
2020-10-19 18:01:58 +0530

    Mern node intro

---
+++ week_27/day_1/assignments/addition.js
@@ -0,0 +1,16 @@
+let addition =(a,b) => {
+    return a + b;
+}
+
+let substraction = (a,b) =>{
+    return a - b;
+}
+let multiplication = (a,b) =>{
+    return a * b
+}
+
+let divide = (a,b) =>{
+    return a / b
+}
+
+module.exports= {addition,substraction,multiplication,divide};
